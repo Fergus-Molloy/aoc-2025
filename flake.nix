3dc447e3703ab752119c890ddc37f3a6af8af5df
Fergus Molloy <fergus@molloy.xyz>
2025-11-28 10:02:05 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Flake for Advent of Code 2025";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      packages = forAllSystems (
+        { pkgs }:
+        rec {
+          aoc = pkgs.buildGoModule {
+            pname = "aoc2026";
+            version = "1.0.0";
+            src = ./.;
+            vendorHash = null;
+          };
+          default = aoc;
+        }
+      );
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go ];
+          };
+        }
+      );
+    };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module fergus.molloy.xyz/aoc2026
+
+go 1.25.4
