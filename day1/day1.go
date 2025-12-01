47756e919b9f6121e49d842ad57dc40178b9c733
Fergus Molloy <fergus@molloy.xyz>
2025-12-01 14:06:26 +0000

    add some more edge case tests and a better algo for pt 2

--- day1/day1.go
+++ day1/day1.go
@@ -14,25 +14,6 @@ func init() {
 	inp = strings.TrimRight(inp, "\n")
 }
 
-func addToDial(d, v int) (int, int) {
-	dial := d + v
-	crossed := false
-	if dial >= 100 {
-		dial -= 100
-		crossed = true
-	}
-	if dial <= -1 {
-		dial += 100
-		crossed = true
-	}
-
-	if (crossed && d != 0) || dial == 0 {
-		return dial, 1
-	}
-
-	return dial, 0
-}
-
 func Pt1() (int, error) {
 	dial := 50
 	zeroCount := 0
@@ -52,21 +33,20 @@ func Pt1() (int, error) {
 		if absValue > 100 {
 			for range absValue / 100 {
 				if value > 0 {
-					dial, _ = addToDial(dial, 100)
+					dial, _ = incrementDial(dial, 100)
 					value -= 100
 				} else {
-					dial, _ = addToDial(dial, -100)
+					dial, _ = incrementDial(dial, -100)
 					value += 100
 				}
 			}
 		}
 
-		dial, _ = addToDial(dial, value)
+		dial, _ = incrementDial(dial, value)
 
 		if dial == 0 {
 			zeroCount += 1
 		}
-
 	}
 
 	return zeroCount, nil
@@ -82,32 +62,66 @@ func Pt2() (int, error) {
 		if err != nil {
 			return 0, fmt.Errorf("Failed to convert str to int: %s", l[1:])
 		}
-		absValue := value
 		if dir == 'L' {
 			value *= -1
 		}
 
-		// account for multiple rotations of dial in one value
-		var i int
 		var zeros int
-		if absValue > 100 {
-			for range absValue / 100 {
-				if value > 0 {
-					dial, i = addToDial(dial, 100)
-					value -= 100
-				} else {
-					dial, i = addToDial(dial, -100)
-					value += 100
-				}
-				zeros += i
-			}
-		}
-
-		dial, i = addToDial(dial, value)
-		zeros += i
+		dial, zeros = addToDial(dial, value)
 
 		zeroCount += zeros
 	}
 
 	return zeroCount, nil
+}
+
+// addToDial simulate turning the dial as many clicks as passed in
+func addToDial(d, v int) (int, int) {
+	absValue := v
+	if absValue < 0 {
+		absValue = -absValue
+	}
+
+	zeros := (absValue / 100)
+	newD := d + (v % 100)
+
+	switch {
+	case newD == 0:
+		if d == 0 {
+			return newD, zeros
+		} else {
+			return newD, zeros + 1
+		}
+	case newD > 99:
+		return newD - 100, zeros + 1
+	case newD < 0:
+		newD += 100
+		if d == 0 {
+			return newD, zeros
+		} else {
+			return newD, zeros + 1
+		}
+	default:
+		return newD, zeros
+	}
+}
+
+// incrementDial simulate turning the dial no more than 100 clicks
+func incrementDial(d, v int) (int, int) {
+	dial := d + v
+	crossed := false
+	if dial >= 100 {
+		dial -= 100
+		crossed = true
+	}
+	if dial <= -1 {
+		dial += 100
+		crossed = true
+	}
+
+	if (crossed && d != 0) || dial == 0 {
+		return dial, 1
+	}
+
+	return dial, 0
 }
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -3,21 +3,131 @@ package day1
 import "testing"
 
 func TestDay1Pt1(t *testing.T) {
+	t.Parallel()
+
 	v, err := Pt1()
 	if err != nil {
-		t.Fatalf("Failed to run Day 1 Pt1: %v", err)
+		t.Fatalf("failed to run day 1 pt1: %v", err)
 	}
 	if v != 1026 {
-		t.Fatalf("Incorrect answer for Day 1 Pt 1\nwant: 1026\n got: %d", v)
+		t.Fatalf("incorrect answer for day 1 pt 1\nwant: 1026\n got: %d", v)
 	}
 }
 
 func TestDay1Pt2(t *testing.T) {
+	t.Parallel()
+
 	v, err := Pt2()
 	if err != nil {
-		t.Fatalf("Failed to run Day 1 Pt2: %v", err)
+		t.Fatalf("failed to run day 1 pt2: %v", err)
 	}
 	if v != 5923 {
-		t.Fatalf("Incorrect answer for Day 1 Pt 2\nwant: 5923\n got: %d", v)
+		t.Fatalf("incorrect answer for day 1 pt 2\nwant: 5923\n got: %d", v)
+	}
+}
+
+func TestAddToDial(t *testing.T) {
+	td := []struct {
+		name                             string
+		dial, value, wantDial, wantZeros int
+	}{
+		{
+			name:      "R50 lands on zero",
+			dial:      50,
+			value:     50,
+			wantDial:  0,
+			wantZeros: 1,
+		},
+		{
+			name:      "L50 lands on zero",
+			dial:      50,
+			value:     -50,
+			wantDial:  0,
+			wantZeros: 1,
+		},
+		{
+			name:      "R50 passes zero",
+			dial:      75,
+			value:     50,
+			wantDial:  25,
+			wantZeros: 1,
+		},
+		{
+			name:      "L50 passes zero",
+			dial:      25,
+			value:     -50,
+			wantDial:  75,
+			wantZeros: 1,
+		},
+		{
+			name:      "R500 passes zero",
+			dial:      25,
+			value:     500,
+			wantDial:  25,
+			wantZeros: 5,
+		},
+		{
+			name:      "L500 passes zero",
+			dial:      25,
+			value:     -500,
+			wantDial:  25,
+			wantZeros: 5,
+		},
+		{
+			name:      "R550 lands on zero",
+			dial:      50,
+			value:     550,
+			wantDial:  0,
+			wantZeros: 6,
+		},
+		{
+			name:      "L550 lands on zero",
+			dial:      50,
+			value:     -550,
+			wantDial:  0,
+			wantZeros: 6,
+		},
+		{
+			name:      "R100 start and end on zero",
+			dial:      0,
+			value:     100,
+			wantDial:  0,
+			wantZeros: 1,
+		},
+		{
+			name:      "L100 start and end on zero",
+			dial:      0,
+			value:     100,
+			wantDial:  0,
+			wantZeros: 1,
+		},
+		{
+			name:      "L557 dial 0",
+			dial:      0,
+			value:     -557,
+			wantDial:  43,
+			wantZeros: 5,
+		},
+		{
+			name:      "L39 dial 0",
+			dial:      0,
+			value:     -39,
+			wantDial:  61,
+			wantZeros: 0,
+		},
+	}
+
+	for _, tc := range td {
+		t.Run(tc.name, func(t *testing.T) {
+			gotDial, gotZeros := addToDial(tc.dial, tc.value)
+
+			if gotDial != tc.wantDial {
+				t.Fatalf("incorrect value on dial\nwant: %d\n got: %d", tc.wantDial, gotDial)
+			}
+
+			if gotZeros != tc.wantZeros {
+				t.Fatalf("incorrect zeros\nwant: %d\n got: %d", tc.wantZeros, gotZeros)
+			}
+		})
 	}
 }
